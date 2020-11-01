a0fc650466108505b49ada64c3de89f370791422
peyman <lord.akinak@gmail.com>
2020-11-01 13:04:56 +0330

    before merge

--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,12 @@ return [
 	],
 
     'middleware' => [
-        'throttle:60,1'
+        'throttle:60,1',
+        \App\Http\Middleware\EncryptCookies::class,
+        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\Session\Middleware\AuthenticateSession::class,
+        'bindings',
     ],
 
     'prefix' => 'api',
--- src/Signup/VerifyPhoneRequest.php
+++ src/Signup/VerifyPhoneRequest.php
@@ -27,7 +27,7 @@ class VerifyPhoneRequest  extends FormRequest {
             'phone' => 'required|numeric|regex:/(09)[0-9]{9}/|digits:11',
             'key' => 'required|string',
             'captcha' => 'required|captcha_api:'.$this->request->get('key'),
-            'accept_terms' => 'required|boolean',
+            'accept_terms' => 'required|boolean|in:1,true',
         ];
     }
 
